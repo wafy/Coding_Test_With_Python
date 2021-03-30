acf1166f466efa3c2b51d8ad2682d903e441a31a
LDD 최선혁 <sunhyeok.choi@kurlycorp.com>
2021-03-30 15:23:24 +0900

    큰 수의 법칙을 풀어라

---
+++ 3-2.py
@@ -0,0 +1,17 @@
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort() # 입력받은 수 정렬
+first = data[n - 1]     # 가장 큰 수
+second = data[n - 2]    # 두번째로 큰 수
+
+# 가장 쿤 수가 더해지는 횟수 계산
+count = 0
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result = 0
+result = count * first  # 가장 큰 수 더하기
+result += (m - count) * second  # 두 번째로 큰 수 더하기
+
+print(result)   # 최종 답안 출력
